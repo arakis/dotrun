96bd1e46746dea311d3c760096347de420def598
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-12-11 23:04:03 +0100

    refactor

---
+++ src/DotRun.Runtime/Node/Node.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DotRun.Runtime
+{
+    public abstract class Node : INode
+    {
+        public virtual IShell CreateShell(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                name = "cmd";
+
+            switch (name)
+            {
+                case "cmd":
+                    return new CmdShell();
+                case "ps":
+                    return new PowerShell();
+            }
+            throw new Exception();
+        }
+
+        public abstract Task<ProcessResult> ExecuteCommand(NodeCommand cmd);
+        public abstract Task WriteFile(StepContext context, string path, Stream source);
+
+        protected async Task<ProcessResult> ExecuteLocalCommand(NodeCommand cmd)
+        {
+            var result = new ProcessResult();
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = cmd.FileName,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+            };
+            foreach (var arg in cmd.Arguments)
+                startInfo.ArgumentList.Add(arg);
+
+            var process = new Process()
+            {
+                StartInfo = startInfo,
+            };
+
+            var outputCloseEvent = new TaskCompletionSource<bool>();
+
+            process.OutputDataReceived += (s, e) =>
+            {
+                // The output stream has been closed i.e. the process has terminated
+                if (e.Data == null)
+                {
+                    outputCloseEvent.SetResult(true);
+                }
+                else
+                {
+                    cmd.Output.WriteLine(e.Data);
+                }
+            };
+
+            var errorCloseEvent = new TaskCompletionSource<bool>();
+
+            process.ErrorDataReceived += (s, e) =>
+            {
+                // The error stream has been closed i.e. the process has terminated
+                if (e.Data == null)
+                {
+                    errorCloseEvent.SetResult(true);
+                }
+                else
+                {
+                    cmd.Output.ErrorLine(e.Data);
+                }
+            };
+
+            bool isStarted;
+
+            try
+            {
+                isStarted = process.Start();
+            }
+            catch
+            {
+                // Usually it occurs when an executable file is not found or is not executable
+
+                result.Completed = true;
+                result.ExitCode = -1;
+
+                isStarted = false;
+            }
+
+            if (isStarted)
+            {
+                // Reads the output stream first and then waits because deadlocks are possible
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                // Creates task to wait for process exit using timeout
+                var waitForExit = WaitForExitAsync(process, cmd.Timeout);
+
+                // Create task to wait for process exit and closing all output streams
+                var processTask = Task.WhenAll(waitForExit, outputCloseEvent.Task, errorCloseEvent.Task);
+
+                // Waits process completion and then checks it was not completed by timeout
+                if (await Task.WhenAny(Task.Delay(CapTimeout(cmd.Timeout)), processTask) == processTask && waitForExit.Result)
+                {
+                    result.Completed = true;
+                    result.ExitCode = process.ExitCode;
+                }
+                else
+                {
+                    try
+                    {
+                        // Kill hung process
+                        process.Kill();
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static Task<bool> WaitForExitAsync(Process process, TimeSpan timeout)
+        {
+            return Task.Run(() => process.WaitForExit((int)CapTimeout(timeout).TotalMilliseconds));
+        }
+
+        private static TimeSpan CapTimeout(TimeSpan ts)
+        {
+            if (ts.TotalMilliseconds > int.MaxValue)
+                return TimeSpan.FromMilliseconds(int.MaxValue);
+            return ts;
+        }
+
+    }
+
+}
