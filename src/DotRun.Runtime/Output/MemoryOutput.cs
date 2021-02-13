013103669113c424da6214ffd89e913996fc1d10
Sebastian Loncar <sebastian.loncar@gmail.com>
2021-02-13 21:04:05 +0100

    Logger clean up

--- src/DotRun.Runtime/Output/MemoryOutput.cs
+++ src/DotRun.Runtime/Output/MemoryOutput.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace DotRun.Runtime
 {
@@ -11,14 +12,14 @@ namespace DotRun.Runtime
     public class MemoryOutput : Output
     {
 
-        //public List<LogItem> Items { get; } = new List<LogItem>();
-        //internal IEnumerable<LogItem> InfoItems { get; private set; }
-        //internal IEnumerable<LogItem> ErrorItems { get; private set; }
+        public List<LogEntry<object>> Items { get; } = new List<LogEntry<object>>();
+        internal IEnumerable<LogEntry<object>> InfoItems { get; private set; }
+        internal IEnumerable<LogEntry<object>> ErrorItems { get; private set; }
 
         public MemoryOutput()
         {
-            //InfoItems = Items.Where(x => x.LogLevel == LogLevel.Information);
-            //ErrorItems = Items.Where(x => x.LogLevel == LogLevel.Error);
+            InfoItems = Items.Where(x => x.LogLevel == LogLevel.Information);
+            ErrorItems = Items.Where(x => x.LogLevel == LogLevel.Error);
         }
 
         public List<string> InfoLines { get; } = new List<string>();
@@ -26,6 +27,7 @@ namespace DotRun.Runtime
 
         public override void Log<TState>(LogLevel logLevel, EventId eventId, TState state, System.Exception exception, System.Func<TState, System.Exception, string> formatter)
         {
+            Items.Add(new LogEntry<object>(logLevel, null, eventId, state, exception, (st, ex) => formatter((TState)st, ex)));
             foreach (var line in SplitLine(formatter(state, exception)))
             {
                 if (logLevel == LogLevel.Error)
